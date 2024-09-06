28752c66dc544395705e287557c31b29a04a332f
Server Chaush <chaush.s.i20@gmail.com>
2024-09-06 14:31:45 +0300

    fix

--- src/repositories/db/models/word.py
+++ src/repositories/db/models/word.py
@@ -9,4 +9,4 @@ class Word(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     word: Mapped[str] = mapped_column()
     language_id: Mapped[int] = mapped_column()
-    raw_traslation_id: Mapped[int] = mapped_column()
+    raw_translation_id: Mapped[int] = mapped_column()
